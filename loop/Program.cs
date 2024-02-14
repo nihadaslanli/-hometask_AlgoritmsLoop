1c6b7989dbf9b74a7c414791553eb708a8b94f00
nihadaslanli <nihadsasl@code.edu.az>
2024-02-14 19:02:20 +0400

    ff

---
+++ loop/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+#region task1
+//int n = 19;
+//int m = 39;
+//if (n < m)
+// for (int i = n; i < m; i++)
+
+
+//        if (i % 2 == 1)
+//        {
+//            Console.WriteLine(i);
+
+//        }
+#endregion
+#region task2
+//int n = 26
+//    if (n/)
+
+
+
+#endregion
+#region task3
+
+//int b = 4;
+//int a = 4;
+//int sum = 0;
+//if (a % b == 0)
+
+//Console.WriteLine(sum);
+
+
+//else
+//{ Console.WriteLine("eded duzgun deyil"); }
+
+
+
+
+
+#endregion
+#region task4
+//int a = 12;
+//int b = 13;
+//int c = 11;
+
+//int enucuzdeyer = a;
+
+//if(b<enucuzdeyer)
+//{
+//    enucuzdeyer = b;
+//}
+//if (c < enucuzdeyer)
+//{
+//    enucuzdeyer= c;
+//}
+//int toplamqiymet = a + b + c - enucuzdeyer;
+
+//Console.WriteLine(+toplamqiymet);
+
+#endregion
